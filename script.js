6881d4f364b57c56ba65627350217606e6a4b434
Ranielli Soares <ranielli.pinho@outlook.com>
2024-09-06 23:00:39 -0300

    desenvolvimento do site, apresentação ao cliente para aprovação

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function toggleMenu() {
     menu.style.display = 'block';
   }
 }
+
+
+//carousel function
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentIndex);
+});
